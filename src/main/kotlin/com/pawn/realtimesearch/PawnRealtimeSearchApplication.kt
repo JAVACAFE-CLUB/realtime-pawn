b85109682ad88264ab2e873bf47d41e302915beb
pawn <devrunner21@gmail.com>
2025-08-06 22:13:48 +0900

    chore: 프로젝트 초기화

---
+++ src/main/kotlin/com/pawn/realtimesearch/PawnRealtimeSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.pawn.realtimesearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PawnRealtimeSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<PawnRealtimeSearchApplication>(*args)
+}
